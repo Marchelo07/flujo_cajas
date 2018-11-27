60cb6b735d4ac13947963bf3942481f2d331b08e
Marchelo07 <Volibear071990>
2018-11-27 17:38:13 -0500

    actualizacion

--- apps/flujo/urls.py
+++ apps/flujo/urls.py
@@ -4,5 +4,6 @@ from apps.flujo import views
 
 urlpatterns = [
     path('home/', views.Home.as_view(),name = 'view_home'),
+    #path('home1/', views.Home1.as_view(),name = 'view_home'),
 ]
 
--- apps/flujo/views.py
+++ apps/flujo/views.py
@@ -5,6 +5,10 @@ from django.views import generic
 class Home(generic.TemplateView):
     template_name = 'home/login.html'
 
+    #class Home1(generic.TemplateView):
+        #template_name = 'home/prueba.html'
+
+
      #def get_context_data(self, **kwargs):
       #  context = super().get_context_data()
       #  return context
